22517536590e77290307ef4d30e5cf7636aa04f2
spadesk <spadesk1991@gmail.com>
2020-07-09 10:47:41 +0800

    feat: 增加自定义检验tag

--- validate/validator.go
+++ validate/validator.go
@@ -1,6 +1,9 @@
 package validate
 
 import (
+	"regexp"
+	"time"
+
 	"github.com/go-playground/validator"
 	"github.com/pkg/errors"
 
@@ -11,10 +14,27 @@ var v *validator.Validate
 
 func init() {
 	v = validator.New()
+	v.RegisterValidation("YYYMMDD", CheckYYYYMMDD)
 }
 
 func Validate(in interface{}) (err error) {
 	return handleError(in, v.Struct(in))
+}
+
+func CheckYYYYMMDD(fl validator.FieldLevel) bool {
+	value := fl.Field().String()
+	f, err := regexp.MatchString("\\w{4}-\\w{2}-\\w{2}", value)
+	if err != nil {
+		return false
+	}
+	if !f {
+		return false
+	}
+	_, err = time.Parse("2006-0102", value)
+	if err != nil {
+		return false
+	}
+	return true
 }
 
 func handleError(obj interface{}, err error) (e error) {
